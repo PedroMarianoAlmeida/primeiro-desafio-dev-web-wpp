151578c1788a8d81917a1649ba2acdf6d535d96b
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-08-27 21:16:27 -0300

    Criando e importando o CardDeServiços
    Fiz uma pasta porque planejei criar a lógica nesse principal e vou criar mais 2 componentes só pra receber UI, um com os Serviços Especiais e outro com todos os serviços (pode até ser o mesmo componente, mas com dados diferentes, e vou tratar os dados nesse principal)

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import data from "./data";
 import React, { useState } from "react";
 
 import SeletorDeNomes from "./componentes/SeletorDeNomes";
+import CardDeServiços from "./componentes/CardDeServiços.jsx";
 
 const nomes = data.map((dado) => dado.dados.nome);
 
 function App() {
   const [nomeSelecionado, setNomeSelecionado] = useState("");
   console.log(nomeSelecionado);
-  
+
   return (
     <div>
       <h1>1º Desafio Técnico</h1>
       <SeletorDeNomes opções={nomes} setNomeSelecionado={setNomeSelecionado} />
+      <CardDeServiços />
     </div>
   );
 }
---
+++ src/componentes/CardDeServiços.jsx/index.jsx
@@ -0,0 +1,5 @@
+const CardDeServiços = () => {
+  return <p>Card de Serviços</p>;
+};
+
+export default CardDeServiços;
